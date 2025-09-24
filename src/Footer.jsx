6e0048bd809eb46d17ea0983e7e68ce5d1c76b95
yoshbatula <yoshbatula2@gmail.com>
2025-09-24 14:09:35 +0800

    login practice

---
+++ src/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+    return (
+        <footer>
+            <p>&copy; {new Date().getFullYear()} My Website</p>
+        </footer>
+    );
+}
+
+export default Footer
---
+++ src/Login.jsx
@@ -0,0 +1,36 @@
+function Login() {
+    return (
+        <div className="flex flex-col justify-center items-center bg-blue-400 w-150 h-110 rounded-md">
+            <div>
+                <h1 className="text-white text-2xl mb-4">Login</h1>
+            </div>
+            <div className="mb-4">
+                <label htmlFor="email" className="block text-white mb-2 mr-41">Email</label>
+                <input
+                    type="text"
+                    id="email"
+                    className="px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                />
+            </div>
+            <div className="mb-4">
+                <label htmlFor="password" className="block text-white mb-2 mr-35">Password</label>
+                <input
+                    type="password"
+                    id="password"
+                    className="px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                />
+            </div>
+            <div className="mb-3">
+                <form action="#">
+                    <input
+                        type="submit"
+                        value={"Sign-In"}
+                        className="bg-red-500 text-white px-4 py-2 rounded-md cursor-pointer w-30"
+                    />
+                </form>
+            </div>
+        </div>
+    )
+}
+
+export default Login;
