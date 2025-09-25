08c50676cc428551188b36435b17fb514ec2f1d7
yoshbatula <yoshbatula2@gmail.com>
2025-09-25 13:22:36 +0800

    progress learning for components

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,8 +1,9 @@
 import logo from './images/Umerch-Logo.png'
+
 function Card() {
     return (
         <div className="card">
-            <img src={logo} alt="" />
+            <img src={logo} alt="logo" className='card-image' />
             <h2>Card Components</h2>
         </div>
     )
