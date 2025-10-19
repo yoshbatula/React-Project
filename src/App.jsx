274638af7512da5d8f474dab52f1399043ffca5c
yoshbatula <yoshbatula2@gmail.com>
2025-10-20 02:44:14 +0800

    trying for routing

--- src/App.jsx
+++ src/App.jsx
@@ -9,16 +9,17 @@ import Navbar from './navbar.jsx'
 import Food from './food.jsx'
 import Card from './Card.jsx'
 function App() {
-  return (
-    <div>
-        <h1>This is the card</h1>
-        <h1>I don't know what to say</h1>
-        <h1>just for commits</h1>
-        <h1>I don't know</h1>
-        <h1>another for commit</h1>
-        <Card></Card>
-    </div>
-  );
+
+  const name = "";
+
+    return (
+      <>
+        <nav>
+          <Navbar />
+        </nav>
+      </>
+    );
+
 }
 
 export default App
--- src/navbar.jsx
+++ src/navbar.jsx
@@ -1,22 +1,17 @@
 import Logo from './images/Umerch-Logo.png'
+import {BrowserRouter, Routes, Route,Link} from 'react-router-dom';
 
 function navbar() {
     return(
-        <div className="bg-[#9C0306] mb-200 w-420 h-25">
-            <div className='flex flex-row'>
-                 <div className="ml-3">
-                <img className="h-20" src={Logo} alt=""/>
-            </div>
-                <div className='flex flex-row ml-4 mt-8 text-base'>
-                    <nav className='text-white'>
-                        <ul>
-                            <li><a href="#">HOME</a></li>
-                            <li><a href="#">SHOP</a></li>
-                        </ul>
-                    </nav>
-                </div>
-            </div>
-        </div>
+        <BrowserRouter>
+            <nav>
+                <ul>
+                    <li>
+                        
+                    </li>
+                </ul>
+            </nav>
+        </BrowserRouter>
     );
 }
 
